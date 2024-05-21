be5f50f5d8303db7cab724d84a092792b20cba9e
Alex Straight <alex@Alexs-Laptop.local>
2024-05-21 11:32:39 -0700

    added slect memu for classes

--- app/class/[...slug]/layout.tsx
+++ app/class/[...slug]/layout.tsx
@@ -1,6 +1,10 @@
 import ActiveSection from "@/components/class-page/active-section";
 import Sidebar from "@/components/class-page/sidebar";
+import SubTopNav from "@/components/class-page/sub-top-nav";
+import { Button } from "@/components/ui/button";
+import { Select, SelectContent } from "@/components/ui/select";
 import { classOptions } from "@/lib/data";
+import { SelectTrigger } from "@radix-ui/react-select";
 import Link from "next/link";
 import { ReactNode } from "react";
 
@@ -20,7 +24,7 @@ export default function ClassLayout({ children, params }: ClassLayoutProps) {
 
   return (
     <div className="h-screen flex flex-col pt-24">
-      <SubTopNav />
+      <SubTopNav classCode={classCode} />
       <div className="flex flex-row container gap-x-12">
         <Sidebar classCode={classCode} otherRoutes={otherRoutes} />
         <ActiveSection classCode={classCode} />
@@ -30,25 +34,36 @@ export default function ClassLayout({ children, params }: ClassLayoutProps) {
   );
 }
 
-function SubTopNav() {
-  return (
-    <div className="container flex flex-row items-center justify-between mb-8">
-      <h1>
-        <span className="text-red-400">SDSU </span>
-        <span>CS</span>
-      </h1>
-      <ul className="flex flex-row items-center gap-x-2">
-        {classOptions.map((option, idx) => (
-          <li key={idx}>
-            <Link
-              className="text-muted-foreground hover:text-primary-foreground transition-colors duration-150"
-              href={`/class${option.route}/home`}
-            >
-              {option.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+// type SubTopNavProps = {
+//   classCode: string;
+// };
+
+// function SubTopNav({ classCode }: SubTopNavProps) {
+//   return (
+//     <div className="container flex flex-row items-center justify-between mb-8">
+//       <h1>
+//         <span className="text-red-400">SDSU </span>
+//         <span>CS</span>
+//       </h1>
+//       <Select>
+//         <SelectTrigger asChild>
+//           <Button>{classCode}</Button>
+//         </SelectTrigger>
+//         <SelectContent>
+//           <ul className="flex flex-row items-center gap-x-2">
+//             {classOptions.map((option, idx) => (
+//               <li key={idx}>
+//                 <Link
+//                   className="text-muted-foreground hover:text-black transition-colors duration-150"
+//                   href={`/class${option.route}/home`}
+//                 >
+//                   {option.name}
+//                 </Link>
+//               </li>
+//             ))}
+//           </ul>
+//         </SelectContent>
+//       </Select>
+//     </div>
+//   );
+// }
---
+++ components/class-page/sub-top-nav.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { classOptions } from "@/lib/data";
+import { Button } from "../ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import Link from "next/link";
+
+type SubTopNavProps = {
+  classCode: string;
+};
+
+export default function SubTopNav({ classCode }: SubTopNavProps) {
+  return (
+    <div className="container flex flex-row items-center justify-between mb-8">
+      <h1>
+        <span className="text-red-400">SDSU </span>
+        <span>CS</span>
+      </h1>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Class" />
+        </SelectTrigger>
+        <SelectContent>
+          <ul>
+            {classOptions.map((option, idx) => (
+              <li key={idx}>
+                {/* <Link
+                  className="text-muted-foreground hover:text-black transition-colors duration-150"
+                  href={`/class${option.route}/home`}
+                >
+                  {option.name}
+                </Link> */}
+                <SelectItem value={option.name}>{option.name}</SelectItem>
+              </li>
+            ))}
+          </ul>
+          {/* <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem> */}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
+
+//   <ul className="flex flex-row items-center gap-x-2">
+//     {classOptions.map((option, idx) => (
+//       <li key={idx}>
+//         <Link
+//           className="text-muted-foreground hover:text-black transition-colors duration-150"
+//           href={`/class${option.route}/home`}
+//         >
+//           {option.name}
+//         </Link>
+//       </li>
+//     ))}
+//   </ul>
