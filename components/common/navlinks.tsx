4d8c4e11373ab648c18138617ab6367eaf3cc584
jaxonhill <jaxoncharleshill@gmail.com>
2024-05-29 08:21:01 -0700

    Add hover styling for NavLink

--- components/common/navlinks.tsx
+++ components/common/navlinks.tsx
@@ -22,7 +22,7 @@ interface INavLink {
 function NavLink({ href, text }: INavLink) {
 	return (
 		<Link
-			className="font-medium font-sans text-charcoal-950 text-base"
+			className="font-medium font-sans text-charcoal-950 text-base hover:text-charcoal-800 hover:underline"
 			href={href}
 		>
 			{text}
