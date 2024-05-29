44373ed997adb6d1bd383a49f75472eaff6dcbb6
jaxonhill <jaxoncharleshill@gmail.com>
2024-05-29 09:40:25 -0700

    Finish Class Page

--- app/class/page.tsx
+++ app/class/page.tsx
@@ -1,9 +1,14 @@
 import CardsLayout from "@/components/class-page/CardsLayout";
 import ProfessorCard from "@/components/class-page/ProfessorCard";
+import HeadingSection from "@/components/common/HeadingSection";
 
 export default function ClassPage() {
 	return (
-		<div>
+		<div className="flex flex-col gap-8">
+			<HeadingSection
+				mainText="Course Name"
+				subText="Course Description goes here, typically longer than name"
+			/>
 			<CardsLayout>
 				<ProfessorCard
 					professorName="Professor Name"
