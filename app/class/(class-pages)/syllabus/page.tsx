9af5977cca569b50825a3684a25634cbe00645d2
jaxonhill <jaxoncharleshill@gmail.com>
2024-05-31 19:55:47 -0700

    Add syllabus page

---
+++ app/class/(class-pages)/syllabus/page.tsx
@@ -0,0 +1,9 @@
+import ClassPageLayout from "@/components/class-page/ClassPageLayout";
+
+export default function SyllabusPage() {
+	return (
+		<ClassPageLayout headingMainText="Syllabus">
+			<>{/* Syllabus PDF goes here */}</>
+		</ClassPageLayout>
+	);
+}
