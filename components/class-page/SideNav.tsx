23c8822d634d9d5b55275aafc68cee60aedbbbd0
jaxonhill <jaxoncharleshill@gmail.com>
2024-06-02 06:50:49 -0700

    Refactor SideNav with NavItem components and DRY patterns

--- components/class-page/SideNav.tsx
+++ components/class-page/SideNav.tsx
@@ -1,71 +1,44 @@
 "use client";
 
-import { Home, Folder, FileText } from "lucide-react";
+import { Home, Folder, FileText, LucideIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 
 export default function SideNav() {
-	// Get the full pathname
-	const pathname: string = usePathname();
-
-	// Function that checks if a specified subpath is in the full pathname
-	const isActive = (subpath: string) => pathname.includes(subpath);
-
 	return (
 		<div className="hidden w-full lg:flex flex-col gap-4 lg:col-span-3">
-			<button className="w-full flex gap-2 py-1 items-center">
-				<Home
-					className={`${
-						isActive("home")
-							? "stroke-bright-red"
-							: "stroke-charcoal-600"
-					}`}
-				/>
-				<span
-					className={`font-semibold font-inter text-xl ${
-						isActive("home")
-							? "text-bright-red"
-							: "text-charcoal-600"
-					}`}
-				>
-					Home
-				</span>
-			</button>
-			<button className="w-full flex gap-2 py-1 items-center">
-				<Folder
-					className={`${
-						isActive("module")
-							? "stroke-bright-red"
-							: "stroke-charcoal-600"
-					}`}
-				/>
-				<span
-					className={`font-semibold font-inter text-xl ${
-						isActive("module")
-							? "text-bright-red"
-							: "text-charcoal-600"
-					}`}
-				>
-					Modules
-				</span>
-			</button>
-			<button className="w-full flex gap-2 py-1 items-center">
-				<FileText
-					className={`${
-						isActive("syllabus")
-							? "stroke-bright-red"
-							: "stroke-charcoal-600"
-					}`}
-				/>
-				<span
-					className={`font-semibold font-inter text-xl ${
-						isActive("syllabus")
-							? "text-bright-red"
-							: "text-charcoal-600"
-					}`}
-				>
-					Syllabus
-				</span>
-			</button>
+			<NavItem icon={Home} subpath="home" label="Home" />
+			<NavItem icon={Folder} subpath="module" label="Modules" />
+			<NavItem icon={FileText} subpath="syllabus" label="Syllabus" />
 		</div>
 	);
 }
+
+interface INavItem {
+	icon: LucideIcon;
+	subpath: string;
+	label: string;
+}
+
+const NavItem = ({ icon: Icon, subpath, label }: INavItem) => {
+	const pathname: string = usePathname();
+
+	// Function that checks if a specified subpath is in the full pathname
+	const isActive = pathname.includes(subpath);
+
+	return (
+		<button className="w-full flex gap-2 py-1 items-center">
+			<Icon
+				className={`${
+					isActive ? "stroke-bright-red" : "stroke-charcoal-600"
+				}`}
+			/>
+			<span
+				className={`font-semibold font-inter text-xl ${
+					isActive ? "text-bright-red" : "text-charcoal-600"
+				}`}
+			>
+				{label}
+			</span>
+		</button>
+	);
+};
